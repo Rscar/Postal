ce1956a5155ac3157a678de96c52032f95eeaae7
Rscar <scarboro.reid@gmail.com>
2013-08-30 23:33:07 -0400

    changed nothing

--- Postal/src/com/bioh/postal/screens/GameScreen.java
+++ Postal/src/com/bioh/postal/screens/GameScreen.java
@@ -35,9 +35,9 @@ import com.bioh.postal.objects.Player;
 import com.bioh.postal.utilites.BodyLoader;
 
 public class GameScreen extends GenericScreen implements InputProcessor{
-	
+
 	public OrthogonalTiledMapRenderer tiledMapRenderer;
-	
+
 	private Box2DDebugRenderer renderer;
 	private World world;
 	private GenericLevelBuilder levelBuilder;
@@ -46,45 +46,45 @@ public class GameScreen extends GenericScreen implements InputProcessor{
 	private ParticleController particleController;
 	private BodyLoader loader;
 	private Postal postal;
-	
+
 	private SpriteBatch backgroundBatch;
 	private SpriteBatch gameBatch;
 	private SpriteBatch hudBatch;
-	
+
 	private boolean leftPressed;
 	private boolean rightPressed;
-	
+
 	private Integer score;
 	private int currentLevel;
-	
+
 	private ArrayList<GenericObject> objects = new ArrayList<GenericObject>();
 	private ArrayList<BackgroundLayer> backgroundLayers = new ArrayList<BackgroundLayer>();
 
 	private BitmapFont font;
 
 	public GameScreen(int level){
-		
+
 		Gdx.input.setInputProcessor(this);
-		
+
 		backgroundBatch = new SpriteBatch();
 		gameBatch = new SpriteBatch();
 		hudBatch = new SpriteBatch();
-		
+
 		buildWorld(level);
-		
+
 	}
-	
+
 	public void buildWorld(int level){
-		
+
 		postal = Postal.getInstance();
-		
+
 		this.currentLevel = level;
-		
+
 		loader = new BodyLoader(Gdx.files.internal("physics/postal.json"));
-		
+
 		world = new World(new Vector2(0, -9.8f), false);
 		particleController = new ParticleController();
-		
+
 		//will control what level we build
 		//loads a different builder based on level
 		switch (currentLevel){
@@ -97,43 +97,43 @@ public class GameScreen extends GenericScreen implements InputProcessor{
 		case 3:
 			break;
 		}
-		
+
 		//physics renderer, this can be taken out at a later date once we have textures and actual graphics to see where
 		//the physics objects are on the screen
 		renderer = new Box2DDebugRenderer();
-		
+
 		tiledMapRenderer = new OrthogonalTiledMapRenderer(levelBuilder.getMap(), 0.5f);
-		
+
 		gameCameraController = new GameCameraController(this);
 		backgroundCameraController = new BackgroundCameraController(this);
-		
+
 		score = 0;
-		
+
 		font = new BitmapFont();
 
 	}
-	
+
 	public World getWorld(){
 		return world;
 	}
 
 	@Override
 	public void update(float delta) {
-		 
+
 		rightPressed = false;
 		leftPressed = false;
-		
+
 		if(Gdx.input.isKeyPressed(Keys.LEFT)) leftPressed = true;
 		if(Gdx.input.isKeyPressed(Keys.RIGHT)) rightPressed = true;
-		
+
 		//loop through multitouches, if its touched, turn on thrusters accordingly
 		for (int i = 0; i < 4; i++) {
-			
+
 			if (Gdx.input.isTouched(i) == false) continue;
 
 			float x = Gdx.input.getX(i);
 			float y = Gdx.input.getY(i);
-			
+
 			if (x < Gdx.graphics.getWidth()/2){
 				leftPressed = true;
 			}
@@ -142,10 +142,10 @@ public class GameScreen extends GenericScreen implements InputProcessor{
 			}
 
 		}
-		
+
 		levelBuilder.getPlayer().setLeft(leftPressed);
 		levelBuilder.getPlayer().setRight(rightPressed);
-		
+
 		//update each of the dynamic objects
 		for (int i = 0; i < objects.size(); i++){
 			objects.get(i).update();
@@ -153,27 +153,27 @@ public class GameScreen extends GenericScreen implements InputProcessor{
 
 		//update each of the particles
 		particleController.update(delta, levelBuilder.getPlayer());
-		
+
 		//update camera controller separately...its not really a dynamic object
 		gameCameraController.update();
-		
+
 		//update each of the background layers
 		for (int i = 0; i < backgroundLayers.size(); i++){
 			backgroundLayers.get(i).update(gameCameraController.getCamera().position.x + (gameCameraController.getCamera().viewportWidth * gameCameraController.getCamera().zoom) / 2 - levelBuilder.getWidth() / 2, gameCameraController.getCamera().position.y, gameCameraController.getCamera().zoom);
 		}
-		
+
 		cleanupObjects();
 		world.step(1/30f, 2, 6);
-		
-		
+
+
 	}
 
 	@Override
 	public void draw(float delta) {
-		
+
 		Gdx.gl.glClearColor(0.0f, 0.0f, 0.2f, 1.0f);   
 	    Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-	    
+
 	    //start background draw
 	    backgroundBatch.setProjectionMatrix(backgroundCameraController.getCamera().combined);
 	    backgroundBatch.begin();
@@ -182,66 +182,66 @@ public class GameScreen extends GenericScreen implements InputProcessor{
 		}
 	    backgroundBatch.end(); 
 	    //end background draw
-	    
+
 	    //start map draw
 	    tiledMapRenderer.setView((OrthographicCamera) gameCameraController.getCamera());
 	    tiledMapRenderer.render();
 		//renderer.render(world, gameCameraController.getCamera().combined);
 		//end map draw
-		
+
 		//start game draw
 		gameBatch.setProjectionMatrix(gameCameraController.getCamera().combined);
 		gameBatch.begin();
 		for (int i = 0; i < objects.size(); i++){
 			objects.get(i).draw(gameBatch);
 		}
-		
+
 		float offsetX = gameCameraController.getCamera().viewportWidth * gameCameraController.getCamera().zoom/2 - 10;
 		float offsetY = gameCameraController.getCamera().viewportHeight * gameCameraController.getCamera().zoom/2 - 20;
-		
+
 		// Draw score
 		font.draw(gameBatch, score.toString(), gameCameraController.getCamera().position.x - offsetX, gameCameraController.getCamera().position.y - offsetY);
-				
+
 		//draw particles
 		particleController.draw(gameBatch);
 		gameBatch.end();
-		
+
 		//end game draw
 
 	}
-	
+
 	public Player getPlayer(){
 		return levelBuilder.getPlayer();
 	}
-	
+
 	public void incrementScore() {
 		score++;
 	}
-	
+
 	public Map getMap(){
 		return levelBuilder.getMap();
 	}
-	
+
 	public Mothership getMothership() {
 		return levelBuilder.getMothership();
 	}
-	
+
 	public void addObject(GenericObject object){
 		objects.add(object);
 	}
-	
+
 	public void addLayer(BackgroundLayer layer){
 		backgroundLayers.add(layer);
 	}
-	
+
 	public void addParticleEffect(ParticleEffect effect){
 		particleController.addParticleEffect(effect);
 	}
-	
+
 	public void addFixture(Body body, String name, FixtureDef fixtureDef, float width){
 		loader.attachFixture(body, name, fixtureDef, width);
 	}
-	
+
 	public void cleanupObjects() {
 		for (int i = 0; i < objects.size(); i++) {
 			if (objects.get(i).flaggedForDelete) {
